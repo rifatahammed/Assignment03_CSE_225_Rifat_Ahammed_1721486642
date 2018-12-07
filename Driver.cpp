8fcabb0c1f6ed05662f93d4adff9e4f6fec65e9b
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-07 23:08:46 +0600

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -17,4 +17,25 @@ int main()
       phoneCompany.AddCustomer(C);
        phoneCompany.AddCustomer(D);
   
+   A.AddNumberForExistingCustomer("12345678901","01711111111","GP");
+   A.AddNumberForExistingCustomer("12345678901","01811111111","Robi");
+    A.AddNumberForExistingCustomer("12345678901","01511111111","Teletalk");  
+  A.AddNumberForExistingCustomer("12345678901","01211111111","Random");
+  
+  B.AddNumberForExistingCustomer("12345678902","01722222222","GP");
+   B.AddNumberForExistingCustomer("12345678902","01822222222","Robi");
+    B.AddNumberForExistingCustomer("12345678902","01522222222","Teletalk");  
+  B.AddNumberForExistingCustomer("12345678902","01222222222","Random");
+  
+    C.AddNumberForExistingCustomer("12345678903","01733333333","GP");
+   C.AddNumberForExistingCustomer("12345678903","01833333333","Robi");
+    C.AddNumberForExistingCustomer("12345678903","01533333333","Teletalk");  
+  C.AddNumberForExistingCustomer("12345678903","01233333333","Random");
+  
+    D.AddNumberForExistingCustomer("12345678904","01744444444","GP");
+   D.AddNumberForExistingCustomer("12345678904","01844444444","Robi");
+    D.AddNumberForExistingCustomer("12345678904","01544444444","Teletalk");  
+  D.AddNumberForExistingCustomer("12345678904","01244444444","Random");
+  
+   
   
