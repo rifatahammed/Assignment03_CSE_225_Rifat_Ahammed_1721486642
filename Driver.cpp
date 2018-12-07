5c3b1b1e7509b5daa0ea1bc98f1ba77602c0a181
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-07 22:58:08 +0600

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -6,3 +6,15 @@
 #include "PhoneNumber.h"
 #include "SortedType.h"
 #include "CustomerInfo.h"
+
+using namespace std;
+
+int main()
+{
+  Company phoneCompany;
+  phoneCompany.AddCustomer(A);
+    phoneCompany.AddCustomer(B);
+      phoneCompany.AddCustomer(C);
+       phoneCompany.AddCustomer(D);
+  
+  
