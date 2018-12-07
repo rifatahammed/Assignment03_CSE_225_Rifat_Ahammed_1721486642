b3a2a89efed8818cd48a15eb7c08701eba78cb3f
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-07 21:31:31 +0600

    Update PhoneNumber.h

--- PhoneNumber.h
+++ PhoneNumber.h
@@ -1,1 +1,70 @@
+#ifndef PHONENUMBER_H_INCLUDED
+#define PHONENUMBER_H_INCLUDED
 
+#include<string>
+
+class PhoneNumber
+{
+public:
+	PhoneNumber(const std::string& number = {}
+	        , const std::string& operatorName = {})
+		:   digits(number)
+		,   operatorName(operatorName)
+	{
+	}
+
+	PhoneNumber(const PhoneNumber& ob){
+	    this->digits.assign(ob.digits);
+	    this->operatorName.assign(ob.operatorName);
+	}
+
+	~PhoneNumber() {}
+
+	std::string GetNumber() const{
+		return this->digits; 
+	}
+
+	std::string GetOperatorName() const{
+		return this->operatorName; 
+	}
+	
+	void SetOperatorName(const std::string &operatorName) {
+		this->operatorName = operatorName; 
+	}
+
+	bool operator== (const PhoneNumber& rhs){
+        return (this->digits== rhs.GetNumber());
+	}
+
+    bool operator!= (const PhoneNumber& rhs){
+        return (this->digits!= rhs.GetNumber());
+    }
+
+	bool operator<(const PhoneNumber& rhs){
+		return (this->digits < rhs.GetNumber());
+	}
+
+	bool operator>(const PhoneNumber& rhs){
+		return (this->digits > rhs.GetNumber());
+	}
+
+
+	friend std::ostream &operator<<(std::ostream &output, const PhoneNumber &number ){
+        std::string str;
+        str += "---Number:";
+        str += number.digits;
+        str += "|Operator:";
+        str += number.operatorName;
+        str += "\n";
+
+        output << str;
+        return output;
+    }
+
+private:
+	std::string digits;
+	std::string operatorName;
+};
+
+
+#endif //PHONENUMBER_H_INCLUDED
